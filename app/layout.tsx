56e630ada6043ca6bae4ef0d3d5d2fe19ae37f10
kapil <kapilchaudhary8280@gmail.com>
2023-09-04 17:05:14 +0530

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-'use client';
+// 'use client';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ToastContainer } from 'react-toastify';
-import { RecoilRoot } from "recoil";
+// import { ToastContainer } from 'react-toastify';
+// import { RecoilRoot } from "recoil";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,13 +18,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <RecoilRoot>
+    // <RecoilRoot>
       <html lang="en">
         <body className={inter.className}>
-          <ToastContainer />
+          {/* <ToastContainer /> */}
           {children}
           </body>
       </html>
-    </RecoilRoot>
+    // </RecoilRoot>
   )
 }
